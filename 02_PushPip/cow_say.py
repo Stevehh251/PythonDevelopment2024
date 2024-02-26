f41e1753e1665ee6a300b88a4f40dab00ae593cb
Alexander Kustenkov <kustenkovAA@my.msu.ru>
2024-02-26 23:27:46 +0300

    Add text param

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,1 +1,12 @@
-print("Hello world")
+import argparse
+
+from cowsay import cowsay
+
+parser = argparse.ArgumentParser(prog="cow_say", 
+                                description="This alternative to default cowsay")
+
+parser.add_argument("text", type=str, help="The message.")
+
+args = parser.parse_args()
+
+print(cowsay(message=args.text))
