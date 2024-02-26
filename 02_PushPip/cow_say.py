36c7bf17c550e3f1aaa1280b3d16fda69ed802fa
Alexander Kustenkov <kustenkovAA@my.msu.ru>
2024-02-26 23:32:34 +0300

    Add cowfile param

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -5,8 +5,15 @@ from cowsay import cowsay
 parser = argparse.ArgumentParser(prog="cow_say", 
                                 description="This alternative to default cowsay")
 
-parser.add_argument("text", type=str, help="The message.")
+parser.add_argument("text", 
+                    type=str, 
+                    help="The message.")
+
+parser.add_argument("-f", 
+                    help="Option specifies a particular cow picture file (''cowfile'') to use", 
+                    default="default")
 
 args = parser.parse_args()
 
-print(cowsay(message=args.text))
+print(cowsay(message=args.text,
+             cow=args.f))
