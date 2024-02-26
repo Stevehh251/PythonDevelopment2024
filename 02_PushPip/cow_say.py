d71e1b2a8090e1678e2088ff18f9340ea6dca228
Alexander Kustenkov <kustenkovAA@my.msu.ru>
2024-02-26 23:35:29 +0300

    Add eyes param

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -13,7 +13,12 @@ parser.add_argument("-f",
                     help="Option specifies a particular cow picture file (''cowfile'') to use", 
                     default="default")
 
+parser.add_argument("-e", 
+                    help="The cow's eyes.", 
+                    default="oo")
+
 args = parser.parse_args()
 
 print(cowsay(message=args.text,
-             cow=args.f))
+             cow=args.f,
+             eyes=args.e))
